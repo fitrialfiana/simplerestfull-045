cefb554cd257ef2b57a22b6fd6f4acd9ed06e167
fitrialfiana <70354770+fitrialfiana@users.noreply.github.com>
2022-12-05 11:16:35 +0700

    #1 Membuat Project
    Membuat project baru

---
+++ simplerestfull/src/test/java/ws/b/simplerestfull/SimplerestfullApplicationTests.java
@@ -0,0 +1,13 @@
+package ws.b.simplerestfull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplerestfullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
